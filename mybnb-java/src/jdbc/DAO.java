d4f99b1554c8fc48a8d6b5ecf9c9233373723a37
Jason Fong <jfong701@gmail.com>
2019-07-25 23:13:08 -0400

    Implemented a distance based search from a point (Haversine formula) - SQL run from Java

--- mybnb-java/src/jdbc/DAO.java
+++ mybnb-java/src/jdbc/DAO.java
@@ -1,6 +1,5 @@
 package jdbc;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.sql.rowset.CachedRowSet;
 
@@ -14,6 +13,7 @@ public class DAO {
 			db.connect();
 
 			dao.getAllAmenities();
+			dao.getListingsWithinRadius(32.715658, -117.16116, 0.6);
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -24,6 +24,25 @@ public class DAO {
 
 	public void getAllAmenities() throws SQLException {
 		String query = "SELECT * FROM Amenities;";
+		CachedRowSet result = db.execute(query);
+		db.PrintResultSetOutput(result);
+	}
+	
+	public void getListingsWithinRadius(double lati, double longi, double searchRadiusKm) throws SQLException{
+		double earthRadius = 6378.1;
+		String query = "SELECT * FROM Listings "
+				+ "WHERE (2 * " + earthRadius
+				+ " * ASIN( SQRT( POWER( SIN( "
+				+ "(RADIANS(" + lati + ") - "
+				+ "RADIANS(Latitude))/2)"
+				+ ",2)"
+				+ "+ COS(RADIANS(Latitude))"
+				+ "* COS(RADIANS(" + lati + "))"
+				+ "* POWER( SIN( "
+				+ "(RADIANS( " + longi + ") - "
+				+ "RADIANS(Longitude))/2)"
+				+ ",2)"
+				+ "))) > " + searchRadiusKm + ";";
 		CachedRowSet result = db.execute(query);
 		db.PrintResultSetOutput(result);
 	}
